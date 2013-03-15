7c99489d024a807a9d7c336b121988822b958de6
Taiki ONO <taiks.4559@gmail.com>
2013-03-16 01:12:50 +0900

    init

---
+++ recent_faved.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+require 'pry'
+
+Plugin.create :recent_faved do
+  start_time = Time.now
+
+  recent_faved = lambda do |msg|
+    msg.from_me? && !(msg.favorited_by.empty?) && Time.parse(msg.to_hash[:created_at]) > start_time
+  end
+
+  tab :recent_faved, "R" do
+    timeline :recent_faved do
+      order do |message|
+        Time.parse(message.to_hash[:created_at]).strftime("%s").to_i
+      end
+    end
+  end
+
+  on_favorite do |service, user, message|
+    if message.from_me?
+      timeline(:recent_faved) << message
+    end
+  end
+
+  on_message_modified do |message|
+    timeline(:recent_faved) << message if recent_faved.(message)
+  end
+end
---
+++ spec
@@ -0,0 +1,10 @@
+---
+slug: :recent_faved
+depends:
+  mikutter: 0.2.2.1150
+  plugin:
+  - gui
+version: '1.0'
+author: taiki45
+name: recent_faved
+description: show your recent faved tweets
